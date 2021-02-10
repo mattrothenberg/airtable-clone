0e3ee6f64b5df590406e1c0a2c68e54283f2723d
Matt Rothenberg <mattrothenberg@github.com>
2021-02-10 07:48:47 -0500

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,133 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import { useTable, useBlockLayout } from "react-table";
+import { Virtuoso, GroupedVirtuoso } from "react-virtuoso";
+
+import makeData from "../makeData";
+
+const Button = ({ children }) => {
+  return (
+    <button className="h-6 hover:bg-gray-100 rounded text-sm text-gray-700 px-2 focus:ring focus:outline-none font-medium">
+      {children}
+    </button>
+  );
+};
+
+const Table = () => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Row Index",
+        accessor: (row, i) => i,
+      },
+      {
+        Header: "First Name",
+        accessor: "firstName",
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastName",
+      },
+      {
+        Header: "Age",
+        accessor: "age",
+        width: 50,
+      },
+      {
+        Header: "Visits",
+        accessor: "visits",
+        width: 60,
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Profile Progress",
+        accessor: "progress",
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => makeData(10000), []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data }, useBlockLayout);
+
+  const RenderRow = React.useCallback(
+    (index) => {
+      const row = rows[index];
+      prepareRow(row);
+      return (
+        <div {...row.getRowProps({})} className="bg-white">
+          {row.cells.map((cell) => {
+            return (
+              <div {...cell.getCellProps()} className="p-1 border-b">
+                {cell.render("Cell")}
+              </div>
+            );
+          })}
+        </div>
+      );
+    },
+    [prepareRow, rows]
+  );
+
+  return (
+    <div>
+      <div className="bg-white p-3 space-x-4">
+        <Button>Hide fields</Button>
+        <Button>Filter</Button>
+        <Button>Group</Button>
+        <Button>Sort</Button>
+      </div>
+
+      <div {...getTableProps()} className="block">
+        <div {...getTableBodyProps()}>
+          <GroupedVirtuoso
+            style={{ height: "400px" }}
+            groupCounts={[rows.length]}
+            groupContent={() => {
+              return (
+                <div className="">
+                  {headerGroups.map((headerGroup) => (
+                    <div
+                      {...headerGroup.getHeaderGroupProps()}
+                      className="bg-gray-100 border-b border-gray-200"
+                    >
+                      {headerGroup.headers.map((column) => (
+                        <div
+                          {...column.getHeaderProps()}
+                          className="text-sm text-gray-600 p-1"
+                        >
+                          {column.render("Header")}
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+            totalCount={rows.length}
+            itemContent={RenderRow}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div className="p-4 bg-blue-200 h-screen">
+      <div className="bg-white shadow rounded-lg overflow-hidden flex flex-col h-full">
+        <Table />
+      </div>
     </div>
-  )
+  );
 }
